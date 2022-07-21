c32419208701286934021c74eb00f2a39e380680
sameh-dh <samehdh44@gmail.com>
2022-07-21 20:56:17 +0100

    nav bar is more organized

---
+++ client/component/navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+const Navbar =()=>{
+    return(<div>
+  <ul className="nav">
+  <li><a href="/index">Home</a></li>
+  <li><a href="/Diet">About</a></li>
+  <li><a href="/Aboutus">Portfolio</a></li>
+  <li><a href="/signup">Contact</a></li>
+</ul>
+
+    </div>) 
+};
+export default Navbar;
