5c66293a6d20fdec67818b4d06801fe3ba429bdb
sameh-dh <samehdh44@gmail.com>
2022-07-22 10:02:31 +0100

    nav bar organized

--- client/component/navbar.tsx
+++ client/component/navbar.tsx
@@ -4,8 +4,9 @@ const Navbar =()=>{
     return(<div>
   <ul className="nav">
   <li><a href="/index">Home</a></li>
-  <li><a href="/Diet">About</a></li>
-  <li><a href="/Aboutus">Portfolio</a></li>
+  <li><a href="/Diet">Diet</a></li>
+  <li><a href="/Workout*">Workout</a></li>
+  <li><a href="/Aboutus">About us</a></li>
   <li><a href="/signup">Contact</a></li>
 </ul>
 
