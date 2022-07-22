8d57eed434037a2ecf55519e6858793a4fcd657d
haythembensouissi <haythembensouissi2@gmail.com>
2022-07-22 12:18:23 +0100

    worked on the workouts

--- client/pages/oneWorkout/[typeID].tsx
+++ client/pages/oneWorkout/[typeID].tsx
@@ -1,16 +1,16 @@
-export default function Type({diets}:any){
+export default function Type({newworkouts}:any){
     return(
         <div>
                 <ul>
-            {diets.map((diet:any,index:number)=>(
+            {newworkouts.map((workout:any,index:number)=>(
                      // eslint-disable-next-line react/jsx-key
                      <ul className="cards">
                      <li className="cards__item">
                       <div className="card">
-                         <img className="card__image card__image--fence" src={diet.image}/>
+                         <img className="card__image card__image--fence" src={workout.img}/>
                          <div className="card__content">
-                           <div className="card__title">{diet.nameReceipe}</div>
-                           <p className="card__text">{diet.description} </p>
+                           <div className="card__title">{workout.name}</div>
+                           <p className="card__text">{workout.description} </p>
                            <button className="btn btn--block card__btn">Button</button>
                          </div>
                        </div>
@@ -23,10 +23,11 @@ export default function Type({diets}:any){
     )
 }
 export async function getServerSideProps({params}:any) {
- const diets  = await fetch (`http://localhost:2000/api/diets/${params.typeID}`).then(response => response.json())
+ const workouts  = await fetch (`http://localhost:2000/api/workouts/all/one/${params.typeID}`).then(response => response.json())
+const newworkouts=[workouts]
  return {
     props:{
-        diets
+        newworkouts
     }
  }
 }   
