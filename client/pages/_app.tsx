2a5289a261f3513b3c8fd4ef020a213b976eb469
HmidiHoussem <hmidi.houssem@gmail.com>
2022-07-22 12:38:42 +0100

    blogging some css

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,18 +1,21 @@
 import "../styles/globals.css";
 import "../styles/signup.scss";
 // import "../styles/Profile.scss";
-import "../styles/searchbar.scss"
-import "../styles/workout.css"
-import "../styles/diet_cards.css"
-import Layout from '../component/layout'
+import "../styles/searchbar.scss";
+import "../styles/workout.css";
+import "../styles/diet_cards.css";
+import "../styles/blog.css";
+import Layout from "../component/layout";
 
 import type { AppProps } from "next/app";
 import Navbar from "./Navbar";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Layout >  <Component {...pageProps} /></Layout>
-   
-    
-
+  return (
+    <Layout>
+      {" "}
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
